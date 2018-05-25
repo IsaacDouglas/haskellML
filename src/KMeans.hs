dada34e1f8ecc51536f0d33088b65e2044ee6e3e
Wellington Felix <wfmf@cin.ufpe.br>
2018-05-25 14:00:10 -0300

    Corrige função de cálculo de distância

--- src/KMeans.hs
+++ src/KMeans.hs
@@ -7,4 +7,5 @@ type Centroid = [Feature]
 
 -- | Computa a distância 📏 entre duas instâncias
 distance :: Example -> Example -> Float
-distance x1 x2 = sqrt . sum . map (\(a, b) -> a - b) . zip x1 $ x2
+distance x1 x2 = sqrt . sum . map (\(a, b) -> (a - b) ^ 2) . zip x1 $ x2
+
